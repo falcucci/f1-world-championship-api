861f0675e5afb44eb9a60152d2c15449db35c912
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2022-11-11 03:23:48 +0100

    add first tests

--- pages/api/health.js
+++ pages/api/health.js
@@ -1,6 +1,5 @@
 const { StatusCodes, getReasonPhrase } = require('http-status-codes');
-const config = require("config")
 
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   res.status(StatusCodes.OK).json({ status: getReasonPhrase(200) })
 }
