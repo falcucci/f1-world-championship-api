d335b76117aedb1a4dc52b1026b372c5e281e343
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2022-11-11 03:57:20 +0100

    fixing tests

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ `command -v fzf` ]]; then
-  ./node_modules/.bin/mocha $(fzf --filter=/__tests__/) -R spec --exit
+  ./node_modules/.bin/mocha $(fzf --filter=__tests__/) -R spec --exit
 else
-  ./node_modules/.bin/mocha '**/__tests__/*' -R spec --exit
+  ./node_modules/.bin/mocha '__tests__/*' -R spec --exit
 fi
